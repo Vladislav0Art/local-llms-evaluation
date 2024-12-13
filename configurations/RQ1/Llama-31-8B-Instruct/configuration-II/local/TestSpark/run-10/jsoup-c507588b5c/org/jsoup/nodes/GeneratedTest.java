713419cbaea08ba085fb7b7c0ac85ce75135ada0
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-12-13 23:32:39 +0000

    feat: generate Llama-31-8B-Instruct:configuration-II for 10 benchmarks

---
+++ configurations/RQ1/Llama-31-8B-Instruct/configuration-II/local/TestSpark/run-10/jsoup-c507588b5c/org/jsoup/nodes/GeneratedTest.java
@@ -0,0 +1,20 @@
+package org.jsoup.nodes;
+
+import org.jsoup.helper.Validate;
+import org.jsoup.internal.StringUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GeneratedTest {
+
+}
---
+++ configurations/RQ1/Llama-31-8B-Instruct/configuration-II/local/TestSpark/run-8/jsoup-e1880ad73e/org/jsoup/helper/GeneratedTest.java
@@ -0,0 +1,31 @@
+package org.jsoup.helper;
+
+import org.jsoup.Connection;
+import org.jsoup.internal.StringUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.io.UnsupportedEncodingException;
+import java.net.IDN;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GeneratedTest {
+
+    @InjectMocks
+    private UrlBuilder urlBuilder;
+
+}
